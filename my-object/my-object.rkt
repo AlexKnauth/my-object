445c1c379391028279d213075e9389d9794449d4
AlexKnauth <alexander@knauth.org>
2015-01-06 21:22:02 -0500

    objects as dicts, support failure-results
    and rename object-ref etc. to object-ref1 etc. and object-ref* etc. to
    object-ref etc.

--- my-object/my-object.rkt
+++ my-object/my-object.rkt
@@ -4,9 +4,9 @@
          object?
          object-extend
          object-fields
-         object-ref   object-ref*
-         object-set-m object-set-m*
-         object-set   object-set*
+         object-ref1   object-ref
+         object-set-m1 object-set-m
+         object-set1   object-set
          send
          this
          )
@@ -19,6 +19,7 @@
          syntax/parse/define
          unstable/hash
          racket/stxparam
+         racket/dict
          "stuff.rkt"
          (for-syntax racket/base
                      syntax/parse
@@ -47,6 +48,28 @@
     #:methods gen:custom-write
     [(define (write-proc obj out mode)
        (obj-write-proc obj out mode))]
+    #:methods gen:dict
+    [(define (dict-ref obj key [failure (λ () (object-ref-failure obj key))])
+       (object-ref1 obj key #:else failure))
+     (define (dict-set obj k v)
+       (object-set1 obj k v))
+     (define (dict-has-key? obj key)
+       (object-has-field? obj key))
+     (define (dict-iterate-first obj)     (hash-iterate-first (object-fields obj)))
+     (define (dict-iterate-next obj pos)  (hash-iterate-next (object-fields obj) pos))
+     (define (dict-iterate-key obj pos)   (hash-iterate-key (object-fields obj) pos))
+     (define (dict-iterate-value obj pos) (hash-iterate-value (object-fields obj) pos))
+     (define (dict-map obj proc)          (hash-map (object-fields obj) proc))
+     (define (dict-for-each obj proc)     (hash-for-each (object-fields obj) proc))
+     (define (dict-empty? obj)            (hash-empty? (object-fields obj)))
+     (define (dict-count obj)             (hash-count (object-fields obj)))
+     (define (dict-keys obj)              (hash-keys (object-fields obj)))
+     (define (dict-values obj)            (hash-values (object-fields obj)))
+     (define (dict->list obj)             (hash->list (object-fields obj)))
+     (define (in-dict obj)                (in-hash (object-fields obj)))
+     (define (in-dict-keys obj)           (in-hash-keys (object-fields obj)))
+     (define (in-hash-values obj)         (in-hash-values (object-fields obj)))
+     (define (in-dict-pairs obj)          (in-hash-pairs (object-fields obj)))]
     ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -56,7 +79,7 @@
 
 (define-syntax-parser deffld
   [(deffld [id1:id id2:id] ths:id)
-   #'(def-var-like-trans id1 #'(object-ref ths 'id2))]
+   #'(def-var-like-trans id1 #'(object-ref1 ths 'id2))]
   [(deffld id:id ths:id)
    #'(deffld [id id] ths)])
 
@@ -121,47 +144,56 @@
 (define object-proc
   (keyword-case-lambda
    [(obj #:fields _) (object-fields obj)]
-   [(obj fld) (object-ref obj fld)]
-   [(obj fld #:-> v) (object-set obj fld #:-> v)]
-   [(obj fld #:->m m) (object-set-m obj fld #:->m m)]
-   [(obj . flds) (apply object-ref* obj flds)]
-   [(obj #:-> v fld . flds) (apply object-set* obj #:-> v fld flds)]
-   [(obj #:->m m fld . flds) (apply object-set-m* obj #:->m m fld flds)]
+   [(obj fld #:else [failure (λ()(object-ref-failure obj fld))]) (object-ref1 obj fld #:else failure)]
+   [(obj fld #:-> v) (object-set1 obj fld #:-> v)]
+   [(obj fld #:->m m) (object-set-m1 obj fld #:->m m)]
+   [(obj #:else [failure fail-sym] . flds) (apply object-ref obj flds #:else failure)]
+   [(obj #:-> v fld . flds) (apply object-set obj #:-> v fld flds)]
+   [(obj #:->m m fld . flds) (apply object-set-m obj #:->m m fld flds)]
    ))
 
 (define (object-fields obj)
   (force (object-fields-promise obj)))
 
-(define (object-ref obj fld)
-  (hash-ref (object-fields obj) fld))
+(define (object-has-field? obj fld)
+  (hash-has-key? (object-fields obj) fld))
 
-(define (object-ref* obj . flds)
+(define (object-ref1 obj fld #:else [failure (λ () (object-ref-failure obj fld))])
+  (hash-ref (object-fields obj) fld failure))
+
+(define fail-sym (gensym 'failure))
+(define (object-ref obj #:else [failure fail-sym] . flds)
+  (define fail (if (eq? failure fail-sym)
+                   (λ () (object-ref*-failure obj flds))
+                   failure))
   (match flds
     [(list) obj]
     [(cons fld flds)
-     (apply object-ref* (object-ref obj fld) flds)]))
+     (if (object-has-field? obj fld)
+         (apply object-ref (object-ref1 obj fld) flds #:else fail)
+         (object-ref1 obj fld #:else fail))]))
 
-(define (object-set-m obj fld #:->m m)
+(define (object-set-m1 obj fld #:->m m)
   (define ths% (object-λobj obj))
   (define new-ths% (extend-λobject ths% (hasheq fld m)))
   (new-ths%))
 
-(define (object-set obj fld #:-> v)
-  (object-set-m obj fld #:->m (λ (ths) v)))
+(define (object-set1 obj fld #:-> v)
+  (object-set-m1 obj fld #:->m (λ (ths) v)))
 
-(define (object-set-m* obj #:->m m fld . flds)
+(define (object-set-m obj #:->m m fld . flds)
   (match (cons fld flds)
-    [(list fld) (object-set-m obj fld #:->m m)]
+    [(list fld) (object-set-m1 obj fld #:->m m)]
     [(cons fld rst)
-     (define obj.fld (object-ref obj fld))
-     (define new-obj.fld (apply object-set-m* obj.fld #:->m m rst))
-     (object-set obj fld #:-> new-obj.fld)]))
+     (define obj.fld (object-ref1 obj fld))
+     (define new-obj.fld (apply object-set-m obj.fld #:->m m rst))
+     (object-set1 obj fld #:-> new-obj.fld)]))
 
-(define (object-set* obj #:-> v fld . flds)
-  (apply object-set-m* obj #:->m (λ (ths) v) fld flds))
+(define (object-set obj #:-> v fld . flds)
+  (apply object-set-m obj #:->m (λ (ths) v) fld flds))
 
 (define (send obj method . args)
-  (apply (object-ref obj method) args))
+  (apply (object-ref1 obj method #:else (λ () (send-failure obj method))) args))
 
 (define (extend-λobject % hsh)
   (match-define (-λobject %.hsh) %)
@@ -181,6 +213,30 @@
          (fprintf out " [~a ~v]" k v))
        (write-string ">" out)
        (void)]))
+
+(define (object-ref-failure obj fld)
+  (error 'object-ref
+         (string-append
+          "object does not have field" "\n"
+          "  field: ~a" "\n"
+          "  object: ~v")
+         fld obj))
+
+(define (object-ref*-failure obj flds)
+  (error 'object-ref*
+         (string-append
+          "object does not have nested field chain" "\n"
+          "  field chain: ~a" "\n"
+          "  object: ~v")
+         flds obj))
+
+(define (send-failure obj method)
+  (error 'send
+         (string-append
+          "object does not have method" "\n"
+          "  method: ~a" "\n"
+          "  object: ~v")
+         method obj))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
